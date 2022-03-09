67d1efd36ab15d4d388ee56e09ffdc5eee9f7be6
Samantha M. Mendoza <michelle2199_@outlook.com>
2022-03-09 12:00:36 -0800

    Trabajo

---
+++ String/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Nombre;
+            string Ciudad;
+            Console.WriteLine("Escriba su nombre: ");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("\nEscriba su ciudad: ");
+            Ciudad = Console.ReadLine();
+            Console.WriteLine("\nHola " + Nombre + ", Bienvenido(a) a " + Ciudad);
+            Console.WriteLine("\nPresione cualquier tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StringInt/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Nombre;
+            int Edad;
+            Console.WriteLine("Escriba su nombre: ");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("\nIngrese su edad: ");
+            Edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nTu Nombre es: " + Nombre + " y tienes: " + Edad + " años");
+            Console.WriteLine("\nPresiona una tecla para finalizar");
+            Console.ReadKey();
+        }
+    }
+}
